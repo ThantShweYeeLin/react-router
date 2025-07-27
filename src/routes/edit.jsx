96b7d6b2af204b752f4456e47a14453def37a7a6
ThantShweYeeLin <u6632067@au.edu>
2025-07-27 14:17:50 +0700

    Initial commit for react-router project

---
+++ src/routes/edit.jsx
@@ -0,0 +1,105 @@
+import { 
+    Form, 
+    useLoaderData,
+    redirect,
+    useNavigate,
+ } from "react-router-dom";
+ import { updateContact, getContact } from "../contacts";
+
+ export async function action({ request, params }) {
+  const formData = await request.formData();
+  //const firstName = formData.get("first");
+ // const lastName = formData.get("last");
+  const updates = Object.fromEntries(formData);
+
+  /*updates.first; // "Some"
+  updates.last; // "Name"
+   if (params.contactId) {
+    await updateContact(params.contactId, updates);
+    return redirect(`/contacts/${params.contactId}`);
+  } else {
+    const newContact = await createContact(updates);
+    return redirect(`/contacts/${newContact.id}`);
+  }*/
+
+  if (!contactId) {
+    const newContact = await createContact(updates);
+    contactId = newContact.id;
+  } else {
+    await updateContact(contactId, updates);
+  }
+
+  return redirect(`/contacts/${contactId}`);
+}
+
+export async function loader({ params }) {
+   if (!params.contactId) {
+    return { contact: {} }; // return empty contact for new
+  }
+  return { contact: await getContact(params.contactId) };
+}
+
+export default function EditContact() {
+  const { contact } = useLoaderData();
+  const navigate = useNavigate();
+
+  return (
+    <Form method="post" id="contact-form">
+      <p>
+        <span>Name</span>
+        <input
+          placeholder="First"
+          aria-label="First name"
+          type="text"
+          name="first"
+          defaultValue={contact?.first}
+        />
+        <input
+          placeholder="Last"
+          aria-label="Last name"
+          type="text"
+          name="last"
+          defaultValue={contact?.last}
+        />
+      </p>
+      <label>
+        <span>Twitter</span>
+        <input
+          type="text"
+          name="twitter"
+          placeholder="@jack"
+          defaultValue={contact?.twitter}
+        />
+      </label>
+      <label>
+        <span>Avatar URL</span>
+        <input
+          placeholder="https://example.com/avatar.jpg"
+          aria-label="Avatar URL"
+          type="text"
+          name="avatar"
+          defaultValue={contact?.avatar}
+        />
+      </label>
+      <label>
+        <span>Notes</span>
+        <textarea
+          name="notes"
+          defaultValue={contact?.notes}
+          rows={6}
+        />
+      </label>
+      <p>
+        <button type="submit">Save</button>
+        <button
+          type="button"
+          onClick={() => {
+            navigate(-1);
+          }}
+        >
+            Cancel
+        </button>
+      </p>
+    </Form>
+  );
+}
